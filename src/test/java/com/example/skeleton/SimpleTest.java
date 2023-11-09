21091472a5b56bafb0720399f734280326ee2cb2
Nayoon <nayoonk928@gmail.com>
2023-11-10 05:19:12 +0900

    test 자동화 확인

---
+++ src/test/java/com/example/skeleton/SimpleTest.java
@@ -0,0 +1,17 @@
+package com.example.skeleton;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void test_success() {
+        assert(true);
+    }
+
+    @Test
+    void test_failure() {
+        assert(false);
+    }
+
+}
